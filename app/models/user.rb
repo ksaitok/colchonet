9e7bf1163235a8b24ec466676ea1e5159ea91301
ksaitok <kazuma.saito@gmail.com>
2012-12-24 01:23:04 +0900

    antes da senha criptografica

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :bio, :email, :full_name, :location, :password
+  attr_accessible :bio, :email, :full_name, :location, :password, :password_confirmation
   
   validates_presence_of :email, :full_name, :location, :password
   validates_confirmation_of :password
